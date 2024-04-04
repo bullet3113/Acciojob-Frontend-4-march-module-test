b23edbc55bfc92b9b3a10b39967008469b2f5404
bullet3113 <0hitesh.a@gmail.com>
2024-04-04 12:11:52 +0530

    deployment 2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Acciojob-Frontend-4-march-module-test/",
   plugins: [react()],
 })
